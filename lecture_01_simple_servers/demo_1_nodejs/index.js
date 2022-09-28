889eb8fffcba8ef463f759f1ec4c2a6fbd6eeada
Kyle <kmthayer@uw.edu>
2022-09-28 15:21:54 -0700

    added demo 3

--- lecture_01_simple_servers/demo_1_nodejs/index.js
+++ lecture_01_simple_servers/demo_1_nodejs/index.js
@@ -6,3 +6,13 @@ var hours = 24 * days
 var minutes = 60 * hours
 
 console.log("There are " + minutes + " in a year")
+
+// note, these are equivalent:
+
+let example_function = function(a, b) {
+    return a + b
+}
+
+let example_function2 = (a, b) => {
+
+}
