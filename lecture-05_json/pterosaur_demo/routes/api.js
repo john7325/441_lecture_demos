1260055bae6954eda3e6aff90a484b173690460c
Kyle <kmthayer@uw.edu>
2022-10-12 14:43:59 -0700

    filtered pterosaur data befre returing

--- lecture-05_json/pterosaur_demo/routes/api.js
+++ lecture-05_json/pterosaur_demo/routes/api.js
@@ -5,8 +5,18 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/getPterosaurs', async function(req, res) {
   const data = await fs.readFile("data/pterosaur.json")
+  let pterosaurInfo = JSON.parse(data)
 
-  res.send(data);
+  // filter out any without images
+  let filteredPterosaurInfo = pterosaurInfo.filter(onePterosaur => {
+    if(onePterosaur.img != ""){
+        return true
+    }else{
+        return false
+    }
+  })
+
+  res.json(filteredPterosaurInfo);
 });
 
 export default router;
