51ca9ef434364ddc7450b2f5ca434878745b0f7d
Kyle <kmthayer@uw.edu>
2022-10-17 14:16:48 -0700

    start mongodb demo

---
+++ lecture-06_mongo_middleware/demo-2-mongodb/routes/api.js
@@ -0,0 +1,9 @@
+import {promises as fs} from 'fs'
+import usersRouter from './users.js';
+import express from 'express';
+var router = express.Router();
+
+router.use('/users', usersRouter)
+
+
+export default router;
---
+++ lecture-06_mongo_middleware/demo-2-mongodb/routes/users.js
@@ -0,0 +1,20 @@
+import {promises as fs} from 'fs';
+import express from 'express';
+var router = express.Router();
+
+router.post('/', async (req, res) =>{
+  console.log(req.body)
+
+  await fs.writeFile("data/userData.json", JSON.stringify(req.body))
+
+  res.send("success")
+})
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  let userInfo = await fs.readFile("data/userData.json")
+  res.type("json")
+  res.send(userInfo)
+});
+
+export default router;
