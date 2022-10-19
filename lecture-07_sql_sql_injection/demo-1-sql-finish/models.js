9747f647bdf0c6f9824762151e195a0117b48b2e
Kyle <kmthayer@uw.edu>
2022-10-19 11:22:57 -0700

    finish sql demo

--- lecture-07_sql_sql_injection/demo-1-sql-finish/models.js
+++ lecture-07_sql_sql_injection/demo-1-sql-finish/models.js
@@ -1,3 +1,29 @@
-let models = {}
+import sqlite3 from 'sqlite3'
+const sqlite3v = sqlite3.verbose()
+
+// initialize my db
+let models = new sqlite3.Database(':memory:', (err) => {
+    if(err){
+        return console.error(err.message)
+    }
+    console.log("Connected to the in-memory sqlite database")
+})
+
+// initiate data tables
+models.serialize(() => {
+    models
+        .run('CREATE TABLE people(first_name text, last_name text)')
+        .run(`INSERT INTO people(first_name, last_name)
+              VALUES ("Kyle", "Thayer"),
+                     ("Kyle", "Chandler"),
+                     ("Miranda", "Ma"),
+                     ("Jared", "Lim") `)
+        
+        .run('CREATE TABLE secret_table(message text)')
+        .run(`INSERT INTO secret_table(message)
+              VALUES ('The password for Kyle is: pa55w0rd'),
+                     ('The treasure is hidden on the 5th floor.')`)
+
+})
 
 export default models
--- lecture-07_sql_sql_injection/demo-1-sql-finish/routes/v1/controllers/people.js
+++ lecture-07_sql_sql_injection/demo-1-sql-finish/routes/v1/controllers/people.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.send("TODO: Find people in database")
+  let nameSearch = req.query.nameSearch
+  nameSearch = nameSearch ? nameSearch : ""
+
+  //To fix the SQL Injection vulnerability, do this instead:
+  //  req.models.all(`SELECT * FROM people WHERE first_name = ?`, nameSearch,
+  req.models.all(`SELECT * FROM people WHERE first_name = "${nameSearch}"`, 
+    (err, allRows) => {
+      if(err){
+        console.log("db error: " + err)
+        res.send("db error" + err)
+        return
+      }
+      if(!allRows){
+        return ""
+      }
+      let matchingPeople = allRows.map(
+          row => `${row.first_name} ${row.last_name}`)
+        .join("\n")
+      res.send(matchingPeople)
+    })
 });
   
 
