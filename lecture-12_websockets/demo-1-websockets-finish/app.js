b8c51f16e54dc946ecafd52c73dfea4326aabcdc
Kyle <kmthayer@uw.edu>
2022-11-07 14:19:09 -0800

    finish soket demo

--- lecture-12_websockets/demo-1-websockets-finish/app.js
+++ lecture-12_websockets/demo-1-websockets-finish/app.js
@@ -1,10 +1,35 @@
 import express from 'express'
+import enableWs from 'express-ws'
 
 const app = express()
+enableWs(app)
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + "/index.html")
 })
+
+let allSockets = []
+let socketCounter = 1
+
+app.ws('/chatSocket', (ws, res) => {
+    let mySocketNum = socketCounter
+    socketCounter++
+
+    console.log("user " + mySocketNum + " connected")
+    allSockets.push(ws)
+
+    ws.on('message', msg =>{
+        console.log("msg (user "+mySocketNum+"): " + msg)
+        allSockets.forEach( socket => {
+            socket.send(mySocketNum + ": " + msg)
+        })
+    })
+
+    ws.on('close', () => {
+        console.log("user "+ mySocketNum + " disconnected. I should probably remove them from the array")
+    })
+})
+
 
 app.listen(3000, () => {
     console.log("Example app listening at http://localhost:3000")
