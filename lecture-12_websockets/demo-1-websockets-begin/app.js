d0535e11b1eacb9936066b353d3580186a564f4f
john7325 <john7325@uw.edu>
2023-03-17 13:45:24 -0700

    complete websocket tutorial

--- lecture-12_websockets/demo-1-websockets-begin/app.js
+++ lecture-12_websockets/demo-1-websockets-begin/app.js
@@ -1,9 +1,33 @@
 import express from 'express'
+import enableWs from "express-ws"; 
 
 const app = express()
+enableWs(app); 
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + "/index.html")
+})
+
+let allSockets = [];
+let socketCounter = 1;
+
+app.ws("/chatSocket", (ws, res) => { 
+  mySocketNum = socketCounter; 
+  socketCounter++; 
+  
+  console.log("user " + mySocketNum + " connected to the socket")
+  allSockets.push(ws); 
+
+  ws.on('message', msg => { 
+    console.log("msg (user + " + mySocketNum +"):" + msg); 
+    allSockets.forEach( socket => { 
+      socket.send(mySocketNum + ": " + msg); 
+    })
+  })
+
+  ws.on("close", () => { 
+    console.log("user " + mySocketNum + " disconnected"); 
+  })
 })
 
 app.listen(3000, () => {
